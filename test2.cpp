d3d1e677063a247cd771fc5be2a4308d192a3182
你猜 <117290962+GoldenStain@users.noreply.github.com>
2024-02-10 14:43:43 +0800

    。

--- test2.cpp
+++ test2.cpp
@@ -1,103 +1,145 @@
+#include <iostream>
 #include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <vector>
+#define For(i, j, n) for (int i = j; i <= n; ++i)
+using namespace std;
 
-char lookahead;
+const int N = 2e6 + 10;
+typedef long long LL;
+typedef pair<LL, int> PLI;
 
-void error() {
-    printf("Error\n");
-    exit(1);
+int A, B, C, m, num, now, opidx[N]; // now记录当前是第几次操作,opidx记录每个点的最后一次操作处于第几次
+vector<PLI> ops[N/2]; // 记录操作序列
+LL nhp[N];
+
+int xytok(int x, int y, int z)
+{
+    return ((x - 1) * B + (y - 1)) * C + z; 
 }
 
-void match(char c) {
-    if (lookahead == c) {
-        lookahead = getchar();
-    } else {
-        error();
+void op_push(int k, int h)
+{
+    //printf("op push : %d, %d\n", k, h);
+    if(ops[k].empty())
+    {
+        ops[k].push_back({(LL)h, now});
+        opidx[k] = now;
+        return ;
     }
-}
-
-int is_digit(char c) {
-    return isdigit(c);
-}
-
-int is_plus(char c) {
-    return c == '+';
-}
-
-int is_minus(char c) {
-    return c == '-';
-}
-
-int is_times(char c) {
-    return c == '*';
-}
-
-int is_divide(char c) {
-    return c == '/';
-}
-
-int is_expr(char c) {
-    return is_digit(c);
-}
-
-int is_term(char c) {
-    return is_expr(c);
-}
-
-int is_factor(char c) {
-    return is_expr(c);
-}
-
-int expr() {
-    int value = term();
-    while (is_plus(lookahead) || is_minus(lookahead)) {
-        if (is_plus(lookahead)) {
-            match('+');
-            value += term();
-        } else if (is_minus(lookahead)) {
-            match('-');
-            value -= term();
-        }
+    if(opidx[k] != now)
+    {
+       ops[k].push_back({(LL)h, now});
     }
-    return value;
+    else
+    {
+        ops[k].back().first += (LL)h;
+    }
+    opidx[k] = now;
+    //nhp[k] += (LL)h;
 }
 
-int term() {
-    int value = factor();
-    while (is_times(lookahead) || is_divide(lookahead)) {
-        if (is_times(lookahead)) {
-            match('*');
-            value *= factor();
-        } else if (is_divide(lookahead)) {
-            match('/');
-            value /= factor();
-        }
-    }
-    return value;
+void Operate(int x1,int y1, int z1, int x2, int y2, int z2, int h)
+{
+    op_push(xytok(x1, y1, z1), h);
+    op_push(xytok(x2 + 1, y1, z1), -h);
+    op_push(xytok(x1, y1, z2 + 1), -h);
+    op_push(xytok(x2 + 1, y1, z2 + 1), h);
+
+    op_push(xytok(x1, y2 + 1, z1), -h);
+    op_push(xytok(x2 + 1, y2 + 1, z1), h);
+    op_push(xytok(x1, y2 + 1, z2 + 1), h);
+    op_push(xytok(x2 + 1, y2 + 1, z2 + 1), -h);
 }
 
-int factor() {
-    int value;
-    if (is_expr(lookahead)) {
-        value = lookahead - '0';
-        match(lookahead);
-    } else if (lookahead == '(') {
-        match('(');
-        value = expr();
-        match(')');
-    } else {
-        error();
-    }
-    return value;
+void add(LL h[], int x, int y, int z)
+{
+    h[xytok(x, y, z)] += h[xytok(x - 1, y, z)] + h[xytok(x, y - 1, z)] + h[xytok(x, y, z - 1)] 
+                  - h[xytok(x, y - 1, z - 1)] - h[xytok(x - 1, y, z - 1)] - h[xytok(x - 1, y - 1, z)]
+                  + h[xytok(x - 1, y - 1, z - 1)];
 }
 
-int main() {
-    lookahead = getchar();
-    int result = expr();
-    if (lookahead != '\n') {
-        error();
+int find(vector<PLI> &v, int x)
+{
+    int l = 0, r = v.size() - 1;
+    while(l < r)
+    {
+        int mid = l + r + 1 >> 1;
+        if(v[mid].second <= x) l = mid;
+        else r = mid - 1;
     }
-    printf("%d\n", result);
+    return l;
+}
+
+bool check(int x)
+{
+    for(int i = 1; i <= num; i++)
+    {
+        int pos = find(ops[i], x);
+        nhp[i] = ops[i][pos].first;
+    }
+    /*for(int i = 1; i <= num; i++)
+        printf("%d ", nhp[i]);
+    puts("");*/
+    for(int i = 1, cnt = 1; i <= A; i++)
+        for(int j = 1; j <= B; j++)
+            for(int k = 1; k <= C; k++, cnt++)
+            {
+                add(nhp, i, j, k);
+                if(nhp[cnt] < 0ll)
+                {
+                    //printf("destroyed at (%d, %d, %d, %d)\n", i, j, k, x);
+                    return true;
+                }
+            }
+    return false;
+}
+
+int search()
+{
+    int l = 1, r = m;
+    while(l < r)
+    {
+        int mid = l + r >> 1;
+        if(check(mid)) r = mid;
+        else l = mid + 1;
+    }
+    return l;
+}
+
+int main()
+{
+    int tmp;
+    scanf("%d%d%d%d", &A, &B, &C, &m);
+    for (int i = 1; i <= A; i++)
+        for (int j = 1; j <= B; j++)
+            for (int k = 1; k <= C; k++, num++)
+            {
+                scanf("%d", &tmp);
+                // 把初始化也看成一次操作，就不需要特判了
+                Operate(i, j, k, i, j, k, tmp);
+            }
+    now++;
+    int x1, y1, z1, x2, y2, z2, h;
+    for (int i = 1; i <= m; i++, now++)
+    {
+        scanf("%d%d%d%d%d%d%d", &x1, &x2, &y1, &y2, &z1, &z2, &h);
+        Operate(x1, y1, z1, x2, y2, z2, -h);
+    }
+    /*printf("size:\n");
+    for(int i = 1; i <= num; i++)
+        printf("%d ", ops[i].size());
+    puts("");*/
+    for(int i = 1; i <= num; i++)
+            for(int j = 1; j < ops[i].size(); j++)
+                ops[i][j].first += ops[i][j - 1].first;
+    /*check(2);
+    for(int i = 1; i <= num; i++)
+        printf("%lld ", nhp[i]);
+    puts("");
+    exit(0);*/
+    printf("%d\n", search());
     return 0;
 }
