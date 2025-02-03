b0fb92c4a32f62357bdbdfd6ea6c8b8b6979b969
你猜 <117290962+GoldenStain@users.noreply.github.com>
2025-02-03 16:32:52 +0800

    update code

--- leetcode189. 轮转数组.cpp
+++ leetcode189. 轮转数组.cpp
@@ -12,3 +12,18 @@ class Solution {
         for (int i = 0, s = n - k; i < n; i++, s++) nums.push_back(tmp[s]);
     }
 };
+
+// solution 2
+class Solution {
+   public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k %= n;
+        if (!k) return;
+        vector<int> tmp = nums;
+        for (int i = 0, s = n - k; i < n; i++) {
+            nums[i] = tmp[s];
+            s = (s + 1) % n;
+        }
+    }
+};
