29232d1f39459513ca910ff0d5ec89a52d1be546
你猜 <117290962+GoldenStain@users.noreply.github.com>
2025-02-10 15:29:46 +0800

    updated leetcode 42

--- leetcode 42. 接雨水.cpp
+++ leetcode 42. 接雨水.cpp
@@ -17,3 +17,45 @@ public:
     }
 };
 
+// 单调栈
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int top = 0;
+        int n = height.size();
+        std::vector<int> stk(n+1);
+        int ans = 0;
+        for(int i = 0; i < height.size(); i++) {
+            while(top && height[stk[top]] < height[i]) {
+                int mid = height[stk[top--]];
+                if(top == 0)
+                    break;
+                int left = stk[top];
+                ans += (std::min(height[left], height[i]) - mid) * (i - left - 1);
+            }
+            stk[++top] = i;
+        }
+        return ans;
+    }
+};
+
+// 双指针
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        int leftMax = -1, rightMax = -1;
+        int l = 0, r = n - 1;
+        int ans = 0;
+        while(l < r) {
+            leftMax = std::max(leftMax, height[l]);
+            rightMax = std::max(rightMax, height[r]);
+            if (leftMax < rightMax)
+                ans += leftMax - height[l++];
+            else
+                ans += rightMax - height[r--];
+        }
+        return ans;
+    }
+};
+
