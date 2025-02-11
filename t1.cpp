fd33169d7c52f1e12ae62070d65cd879cdc8bcd7
你猜 <117290962+GoldenStain@users.noreply.github.com>
2025-02-12 01:49:35 +0800

    updated readme

--- t1.cpp
+++ t1.cpp
@@ -1,114 +1,85 @@
-#include <iostream>
 #include <stdio.h>
+#include <cassert>
 #include <algorithm>
-#include <string>
-#include <vector>
 #include <cmath>
-#include <string.h>
-#define R(x) x = read()
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <chrono>
+#include <vector>
+
 #define For(i, j, n) for (int i = j; i <= n; ++i)
-using namespace std;
-using namespace std;
-int e1, e2, x1, x2;
-int a1[6], a2[6];
-int t1[5], t2[5];
-int f1[6], f2[6];
-int flag;
-int Min;
-int path1[6];
-int path2[6];
-int enter;
-vector<double> arr;
-void init()
-{
-    cin >> e1 >> e2 >> x1 >> x2;
-    for (int i = 1; i <= 5; i++)
-        cin >> a1[i];
-    for (int i = 1; i <= 5; i++)
-        cin >> a2[i];
-    for (int i = 1; i <= 4; i++)
-        cin >> t1[i];
-    for (int i = 1; i <= 4; i++)
-        cin >> t2[i];
+#ifdef DEBUG
+#define DEBUG_LOG(fmt, ...)                  \
+    do {                                     \
+        fprintf(stderr, fmt, ##__VA_ARGS__); \
+    } while (0);
+#else
+#define DEBUG_LOG(fmt, ...) \
+    do {                    \
+    } while (0);
+#endif
+
+template <typename T>
+inline T read() {
+    T x = 0;
+    int f = 1;
+    char ch = getchar();
+    while (ch < '0' || ch > '9') {
+        if (ch == '-') f = -1;
+        ch = getchar();
+    }
+    while (ch >= '0' && ch <= '9') {
+        x = x * 10 + ch - '0';
+        ch = getchar();
+    }
+    return x * f;
 }
-int main()
-{
-    init();
-    for (int i = 1; i <= 5; i++)
-    {
-        if (i == 1)
-        {
-            f1[i] = e1 + a1[1];
-            f2[i] = e2 + a2[1];
-        }
+
+std::vector<int> arr(101);
+
+int binary_serach(int tar) {
+    int l = 0, r = 100;
+    while(l < r) {
+        int mid = (l + r) >> 1;
+        if (arr[mid] >= tar)
+            r = mid;
         else
-        {
-            f1[i] = min(f1[i - 1] + a1[i], f2[i - 1] + t2[i - 1] + a1[i]);
-            if (f1[i - 1] + a1[i] < f2[i - 1] + t2[i - 1] + a1[i])
-                path1[i] = 1;
-            else
-                path1[i] = 2;
-            f2[i] = min(f2[i - 1] + a2[i], f1[i - 1] + t1[i - 1] + a2[i]);
-            if (f2[i - 1] + a2[i] < f1[i - 1] + t1[i - 1] + a2[i])
-                path2[i] = 2;
-            else
-                path2[i] = 1;
-        }
-        Min = min(f1[5] + x1, f2[5] + x2);
-        if (f1[5] + x1 < f2[5] + x2)
-            flag = 1;
-        else
-            flag = 2;
+            l = mid + 1;
     }
-    for (int i = 1; i <= 5; i++)
-    {
-        cout << f1[i] << " " << f2[i] << endl;
+    if (l == 100)
+        return -114514;
+    return arr[l];
+}
+
+int stl_search(int tar) {
+    auto it = std::lower_bound(arr.begin(), arr.end(), tar);
+    if (it == arr.end())
+        return -114514;
+    return *it;
+}
+
+template <typename Func>
+long long measure_time(Func func, int iterations = 10000000) {
+    using namespace std::chrono;
+    long long total_time = 0;
+    auto start = high_resolution_clock::now();
+    for (int i = 0; i < iterations; ++i) {
+        func(rand() % 100);
     }
-    cout << Min << endl;
-    if (flag == 1)
-        //printf("s1.5 ");
-        arr.push_back(1.5);
-    else if (flag == 2)
-        arr.push_back(2.5);
-    for (int i = 5; i >= 2; i--)
-    {
-        if (flag == 1)
-        {
-            if (path1[i] == 1)
-            {
-                cout << 1 <<endl;
-                //printf("s1.%d ", i - 1);
-                arr.push_back(1 + (i-1)*0.1);
-                flag = 1;
-            }
-            else if (path1[i] == 2)
-            {
-                cout << 2 << endl;
-                //printf("s2.%d ", i - 1);
-                arr.push_back(2 + (i-1)*0.1);
-                flag = 2;
-            }
-        }
-        else if (flag == 2)
-        {
-            if (path2[i] == 1)
-            {
-                cout << 3 << endl;
-                //printf("s1.%d ", i - 1);
-                arr.push_back(1 + (i-1)*0.1);
-                flag = 1;
-            }
-            else if (path2[i] == 2)
-            {
-                cout << 4 << endl;
-                //printf("s2.%d ", i - 1);
-                arr.push_back(2 + (i-1)*0.1);
-                flag = 2;
-            }
-        }
-    }
-    reverse(arr.begin(), arr.end());
-    for(auto i:arr)
-        cout << i << " ";
+    auto end = high_resolution_clock::now();
+    total_time += duration_cast<microseconds>(end - start).count();
+
+    return total_time;  // 返回总时间
+}
+
+int main() {
+    srand(114514);
+    for (int i = 0; i < 100; i++) arr[i] = rand() % 5455;
+    arr[100] = -114514;
+    std::sort(arr.begin(), arr.end());
+    long long time1 = measure_time(&binary_serach);
+    long long time2 = measure_time(&stl_search);
+    std::cout << "my time: " << time1 << std::endl << "stl time: " << time2 << std::endl;
     return 0;
 }
