4d087e0fbfd96122fd9235569400cde620296536
你猜 <117290962+GoldenStain@users.noreply.github.com>
2025-02-13 15:54:00 +0800

    add ACwing 791

--- leetcode 791. 高精度加法.cpp
+++ leetcode 791. 高精度加法.cpp
@@ -1,6 +1,49 @@
 #include <iostream>
 #include <vector>
 
+// solution 1
+
+std::vector<int> add(std::vector<int> &A, std::vector<int> &B) {
+    int i = 0, carry = 0;
+    while (i < A.size()) {
+        if (i < B.size()) {
+            A[i] = A[i] + B[i] + carry;
+            carry = A[i] / 10;
+            A[i] %= 10;
+        }
+        else {
+            A[i] += carry;
+            carry = A[i] / 10;
+            A[i] %= 10;
+        }
+        i++;
+    }
+    if (carry)
+        A.emplace_back(carry);
+    return A;
+}
+
+int main() {
+    std::string a, b;
+    std::vector<int> A, B;
+    std::cin >> a >> b;
+    for(int i = a.size() - 1; i >=0; i--)
+        A.emplace_back(a[i] - '0');
+    for(int i = b.size() - 1; i >= 0; i--)
+        B.emplace_back(b[i] - '0');
+    if (A.size() >= B.size())
+        A = add(A, B);
+    else
+        A = add(B, A);
+    for(int i = A.size() - 1; i >= 0; i--)
+        printf("%c", A[i] + '0');
+    return 0;
+}
+
+// super solution
+#include <iostream>
+#include <vector>
+
 constexpr int Base = 1000000000, Cnt = 9;
 
 std::vector<int> add(std::vector<int> &A, std::vector<int> &B) {
