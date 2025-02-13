ec6a997cce9ea7ca7ed72f807ea8a8cdd1b745a9
你猜 <117290962+GoldenStain@users.noreply.github.com>
2025-02-13 21:09:36 +0800

    changed t1 for acwing 793

--- t1.cpp
+++ t1.cpp
@@ -1,63 +1,100 @@
 #include <iostream>
+#include <stdio.h>
+#include <algorithm>
+#include <string>
+#include <cmath>
 #include <vector>
-#include <cstring>
-using vecInt = std::vector<int>;
 
-constexpr int Base = 1000000000, Cnt = 9;
+#define For(i, j, n) for(int i = j ; i <= n ; ++i)
+#ifdef DEBUG
+#define DEBUG_LOG(fmt, ...)                  \
+do {                  \
+\
+fprintf(stderr, fmt, ##__VA_ARGS__); \
+} while (0);
+#else
+#define DEBUG_LOG(fmt, ...) \
+do {                    \
+} while (0);
+#endif
 
-void Input(vecInt &X, std::string &x) {
+using VecInt = std::vector<int>;
+constexpr int Base = 100000, Cnt = 5;
+
+template <typename T>
+inline T read()
+{
+    T x = 0;
+    int f = 1; char ch = getchar();
+    while(ch < '0' || ch > '9')
+    {
+        if(ch == '-') f = -1;
+        ch = getchar();
+    }
+    while(ch >= '0' && ch <= '9')
+    {
+       x = x * 10 + ch - '0';
+       ch = getchar();
+    }
+    return x * f;
+}
+
+void Input(VecInt &X, std::string &x) {
     int st = 0;
-    // 处理前导零
     while(st < x.size() && x[st] == '0')
         st++;
-    for(int i = x.size() - 1, sum = 0, t = 1, j = 0; i >= st; i--) {        
+    for(int i = x.size() - 1, sum = 0, j = 0, t = 1; i >= st; i--) {
         sum = sum + (x[i] - '0') * t;
         t *= 10;
         j++;
         if (j == Cnt || i == st) {
             X.push_back(sum);
-            sum=0;j=0;t=1;
+            sum = 0;
+            t = 1;
+            j = 0;
         }
     }
 }
 
-bool cmp(vecInt &A, vecInt &B) {
-    if (A.size() != B.size())
-        return A.size() > B.size();
-    // 要注意这里反转过了，所以高位在后面.
-    for(int i = A.size() - 1; i >= 0; i--)
-        if (A[i] != B[i])
-            return A[i] > B[i];
-    return true;
-}
-
-vecInt sub(vecInt &A, vecInt &B) {
+VecInt mul(VecInt &A, int b) {
     int t = 0;
-    for(int i = 0; i < A.size(); i++) {
-        int b = i < B.size() ? B[i] : 0;
-        t = A[i] - t - b;
-        A[i] = (t + Base) % Base;
-        t = t >=0 ? 0: 1;
+    for(int i = 0; i < A.size() || t; i++) {
+        if (i < A.size())
+            t += A[i] * b;
+        if (i >= A.size())
+            A.emplace_back(0);
+        A[i] = t % Base;
+        t /= Base;
     }
     return A;
 }
 
-int main() {
+VecInt superMul(VecInt A, VecInt B) {
+    return A;
+}
+
+int main()
+{
     std::string a, b;
+    int *num_b = nullptr;
+    VecInt A;
     std::cin >> a >> b;
-    vecInt A, B;
     Input(A, a);
-    Input(B, b);
-    if (cmp(A, B))
-        A = sub(A, B);
-    else {
-        printf("-");
-        A = sub(B, A);
+    if (b.size() < 6)
+        num_b = new int (std::stoi(b));
+    // super
+    if (!num_b) {
+        VecInt B;
+        Input(B, b);
+        A = superMul(A,B);
     }
-    // 处理前导零，要注意记得加上这个size()的判断，如果整个结果里面只剩下一个0，说明结果真的就是0，不能再pop了.
-    while(A.size() && A.back() == 0)
-        A.pop_back();
+    // normal
+    else {
+        A = mul(A, *num_b);
+    }
+    delete num_b;
     printf("%d", A[A.size() - 1]);
     for(int i = A.size() - 2; i >= 0; i--)
-        printf("%09d", A[i]);
+        printf("%0*d", Cnt, A[i]);
+    return 0;
 }
