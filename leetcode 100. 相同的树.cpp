a81ccb99a3181a49eb5861f96cd3d14ed5e02d0b
你猜 <117290962+GoldenStain@users.noreply.github.com>
2025-02-17 22:50:04 +0800

    add leetcode 100

---
+++ leetcode 100. 相同的树.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    public:
+        bool isSameTree(TreeNode* p, TreeNode* q) {
+            if (!p)
+                return !q;
+            if (!q)
+                return !p;
+            if (p->val != q->val)
+                return false;
+            bool l = isSameTree(p->left, q->left), r = isSameTree(p->right, q->right);
+            return l && r;
+        }
+    };
