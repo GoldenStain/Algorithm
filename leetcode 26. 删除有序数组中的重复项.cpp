52be78c3b766295aaabb7c083bc2ecdf698ed8a9
你猜 <117290962+GoldenStain@users.noreply.github.com>
2025-02-26 22:59:38 +0800

    updated leetcode 26 80

--- leetcode 26. 删除有序数组中的重复项.cpp
+++ leetcode 26. 删除有序数组中的重复项.cpp
@@ -1,13 +1,15 @@
 // solution 1
 class Solution {
-public:
-    int removeDuplicates(vector<int>& nums) {
-        std:vector<int>::iterator res = std::unique(nums.begin(), nums.end());
-        nums.erase(res, nums.end());
-        return res - nums.begin();
-    }
-};
-
+    public:
+        int removeDuplicates(vector<int>& nums) {
+            int k = 0;
+            for(int i = 0; i < nums.size(); i++) {
+                if (i == 0 || nums[i] != nums[i - 1]) 
+                    nums[k++] = nums[i];
+            }
+            return k ;
+        }
+    };
 // solution 2
 class Solution {
 public:
--- leetcode 80. 删除有序数组中的重复项 II.cpp
+++ leetcode 80. 删除有序数组中的重复项 II.cpp
@@ -12,3 +12,24 @@ class Solution {
         return k;
     }
 };
+
+// solution 2
+class Solution {
+    public:
+        int removeDuplicates(vector<int>& nums) {
+            int cnt = 0, k = 0;
+            for(int i = 0; i < nums.size(); i++) {
+                if (!i || nums[i] == nums[i - 1])
+                    cnt++;
+                else {
+                    for(int j = 0; j < min(cnt, 2); j++) {
+                        nums[k++] = nums[i - 1];
+                    }
+                    cnt = 1;
+                }
+            }
+            for (int j = 0; j < min(cnt, 2); j++)
+                nums[k++] = nums[nums.size() -1];
+            return k;
+        }
+    };
