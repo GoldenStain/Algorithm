02d2d2a11ca516761d072cdf45df10341ea943b0
你猜 <117290962+GoldenStain@users.noreply.github.com>
2025-02-26 23:11:27 +0800

    updated leetcode 19

--- leetcode 19. 删除链表的倒数第 N 个结点.cpp
+++ leetcode 19. 删除链表的倒数第 N 个结点.cpp
@@ -27,3 +27,32 @@ class Solution {
             return dummy->next;
         }
     };
+
+// solution 2
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+    public:
+        ListNode* removeNthFromEnd(ListNode* head, int n) {
+            if (!head)
+                return head;
+            ListNode *dummy = new ListNode(-1, head);
+            ListNode *slow = dummy, *fast = head;
+            for(int i = 0; i < n; i++)
+                fast = fast->next;
+            while(fast) {
+                slow = slow->next;
+                fast = fast->next;
+            }
+            slow->next = slow->next->next;
+            return dummy->next;
+        }
+    };    
