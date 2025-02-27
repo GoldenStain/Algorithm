8a73adbab562ccba1157cc8c8453553621069d49
你猜 <117290962+GoldenStain@users.noreply.github.com>
2025-02-27 21:31:11 +0800

    updated leetcode 112

--- leetcode 112. 路径总和.cpp
+++ leetcode 112. 路径总和.cpp
@@ -45,3 +45,15 @@ class Solution {
     };
 
 // 正解DFS
+class Solution {
+    public:
+        bool hasPathSum(TreeNode* root, int targetSum) {
+            if (!root)
+                return false;
+            if(!root->left && !root->right)
+                return root->val == targetSum;
+            return hasPathSum(root->left, targetSum - root->val) || hasPathSum(root->right, targetSum - root->val);
+        }
+    };
+
+// 正解BFS
