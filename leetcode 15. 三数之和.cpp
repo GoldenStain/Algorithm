f047a644fa2c6f7dd4bbb33f867cf8baf7d40829
你猜 <117290962+GoldenStain@users.noreply.github.com>
2025-02-28 19:58:04 +0800

    updated leetcode 15.

--- leetcode 15. 三数之和.cpp
+++ leetcode 15. 三数之和.cpp
@@ -30,3 +30,41 @@ class Solution {
             return ans;
         }
     };
+
+// 双指针while写法
+// 因为这里都是用while写的，所以所有的操作都要注意不能越界，要加上小于n的判断。
+class Solution {
+    public:
+        vector<vector<int>> threeSum(vector<int>& nums) {
+            std::sort(nums.begin(), nums.end());
+            vector<vector<int>> ans;
+            int n = nums.size();
+            for(int i = 0; i <= n - 3; i++) {
+                if (i && nums[i] == nums[i - 1])
+                    continue;
+                int j = i + 1, k = n - 1;
+                while(j < k) {
+                    // 注意不仅仅i需要判重，j也需要，而当i和j都判重之后，k作为剩下的那个数，就不需要判重了。
+                    // 而且要加一个j > i + 1的条件，因为j == i + 1的时候，刚开始，这时候j和i相等是没问题的
+                    while (j < n && j > i + 1 && nums[j] == nums[j - 1])
+                        j++;
+                    // 我们找i < j < k的三元组，如果相遇了就说明不再会出现这样的三元组了，可以结束了
+                    // 类似于快排当中的if(i < j) swap(a[i], a[j]);
+                    if (j >= k)
+                        break;
+                    int sum = nums[j] + nums[k];
+                    if(sum == -nums[i]) {
+                        ans.push_back({nums[i], nums[j], nums[k]});
+                        j++; k--;
+                    }
+                    else if (sum < -nums[i])
+                        j++;
+                    else
+                        k--;
+                }
+            }
+            return ans;
+        }
+    };
+
+// 双指针for写法，比while写法快一些
