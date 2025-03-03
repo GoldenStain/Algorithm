eb1a052c37bc368d17f92f31bc1879405d42e7f1
你猜 <117290962+GoldenStain@users.noreply.github.com>
2025-03-03 15:11:32 +0800

    add luogu P10387 [蓝桥杯 2024 省 A] 训练士兵.cpp

--- luogu P10387 [蓝桥杯 2024 省 A] 训练士兵.cpp
+++ luogu P10387 [蓝桥杯 2024 省 A] 训练士兵.cpp
@@ -1,3 +1,4 @@
+// 正解
 #include <iostream>
 #include <stdio.h>
 #include <algorithm>
@@ -65,5 +66,61 @@ int main()
         alone_cost -= alone_price[i];
     }
     std::cout << group_cost + total_cost;
+    return 0;
+}
+
+// my solution
+typedef long long LL;
+
+constexpr int N = 1e5 + 5;
+
+struct Soldier {
+    int p, c;
+    Soldier(int p, int c) : p(p), c(c) {}
+	Soldier() : p(0), c(0) {}
+    bool operator<(const Soldier& other) const { return c < other.c; }
+} soldiers[N];
+
+int n;
+LL S, sumofP[N], sumofPC[N];
+
+std::set<int> setofC;
+
+int find(int x) {
+	int l = 1, r = n;
+	while(l < r) {
+		int mid = l + r >> 1;
+		if(soldiers[mid].c < x)
+			l = mid + 1;
+		else 
+			r = mid;
+	}
+	return l;
+} // 第一个大于等于x的值的位置
+
+LL sumFromVal(int val) {
+	int pos = find(val);
+	return (sumofPC[n] - sumofPC[pos - 1]) - (LL)val * (sumofP[n] - sumofP[pos - 1]);
+}
+
+int main() {
+    n = read<int>();
+    S = read<LL>();
+    for (register int i = 1; i <= n; i++) {
+        int p = read<int>(), c = read<int>();
+        soldiers[i] = Soldier(p, c);
+        setofC.insert(c);
+    }
+    std::sort(soldiers + 1, soldiers + n + 1);
+    for (int i = 1; i <= n; i++) {
+        sumofP[i] = sumofP[i - 1] + (LL)soldiers[i].p;
+		sumofPC[i] = sumofPC[i - 1] + (LL)soldiers[i].c * (LL)soldiers[i].p;
+    }
+    LL ans = 1000000000000000000LL;
+    for (auto val : setofC) {
+		LL tmpAns = S * val + sumFromVal(val);
+		ans = std::min(ans, tmpAns);
+    }
+	printf("%lld\n", ans);
     return 0;
 }
