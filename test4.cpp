f3fa4fa3daef23c4981428a515f5d051ad2128e8
你猜 <117290962+GoldenStain@users.noreply.github.com>
2025-03-07 21:41:17 +0800

    226. 翻转二叉树

--- test4.cpp
+++ test4.cpp
@@ -1,10 +1,11 @@
 #include <stdio.h>
+
 #include <algorithm>
 #include <cmath>
 #include <iostream>
+#include <list>
 #include <set>
 #include <string>
-#include <list>
 #include <unordered_map>
 
 #define For(i, j, n) for (int i = j; i <= n; ++i)
@@ -37,13 +38,59 @@ inline T read() {
 
 struct Node {
     int key, value, freq;
-    Node(int k, int v, int f): key(k), value(v), freq(f) {}
+    Node(int k, int v, int f) : key(k), value(v), freq(f) {}
 };
 class LFUCache {
    public:
-    
+    LFUCache(int capacity) : _capacity(capacity) {}
+
+    int get(int key) {
+        auto find_res = key_table.find(key);
+        if (find_res == key_table.end()) {
+            return -1;
+        }
+        auto target = find_res->second;
+        int value = target->value;
+        reposition(target, value);
+        return value;
+    }
+
+    void put(int key, int value) {
+        auto find_res = key_table.find(key);
+        if (find_res == key_table.end()) {
+            try_remove();
+            Node node = Node(key, value, 1);
+            min_freq = 1;
+            freq_table[1].push_front(node);
+            key_table[key] = freq_table[1].begin();
+        } else {
+            reposition(find_res->second, value);
+        }
+    }
+
+    void try_remove() {
+        if (key_table.size() < _capacity) return;
+        Node node = freq_table[min_freq].back();
+        freq_table[min_freq].pop_back();
+        key_table.erase(node.key);
+        if (freq_table[min_freq].size() == 0)
+            freq_table.erase(min_freq), min_freq++;
+    }
+
+    void reposition(std::list<Node>::iterator it, int value) {
+        int key = it->key, freq = it->freq;
+        freq_table[freq].erase(it);
+        if (freq_table[freq].size() == 0) {
+            if (freq == min_freq) min_freq++;
+        }
+        freq++;
+        Node node = Node(key, value, freq);
+        freq_table[freq].push_front(node);
+        key_table[key] = freq_table[freq].begin();
+    }
+
    private:
-   // 这里的key_table键值要选择迭代器而不是Node本身，是因为我们没法通过Node本身去高效地访问一个std::list，就像我们手写的链表要用指针去访问一样，std::list要高效访问，就需要用到迭代器。
+    // 这里的key_table键值要选择迭代器而不是Node本身，是因为我们没法通过Node本身去高效地访问一个std::list，就像我们手写的链表要用指针去访问一样，std::list要高效访问，就需要用到迭代器。
     std::unordered_map<int, std::list<Node>::iterator> key_table;
     std::unordered_map<int, std::list<Node>> freq_table;
     int _capacity, min_freq = 0;
@@ -56,4 +103,12 @@ class LFUCache {
  * obj->put(key,value);
  */
 
-int main() { return 0; }
+int main() {
+    std::list<int> l(10, 2);
+    auto it = l.begin();
+    std::advance(it, 1);
+    l.insert(it, 3);
+    std::advance(it, -1);
+    std::cout << *it << std::endl;
+    return 0;
+}
