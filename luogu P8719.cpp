05d28a7908fa0bbbfb1f57da63075c2a60d2dc1a
你猜 <117290962+GoldenStain@users.noreply.github.com>
2025-03-08 20:53:24 +0800

    add luogu P8719 [蓝桥杯 2020 省 AB2] 字串排序

--- luogu P8719.cpp
+++ luogu P8719.cpp
@@ -1,18 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
+// sum是全局的字母个数，cnt是临时的，用来在check函数中统计数量。
 int V , len , now , cnt[27] , sum[27];
 int get_max(int len){
+    // 这个公式当中，加号前后的部分，各自会被对方计算一次，所以结果要除以二。
     return ((len - (len / 26 + 1)) * (len / 26 + 1) * (len % 26) + (26 - len % 26) * (len / 26) * (len - len / 26)) / 2;
 }
 bool check(int x , int n){
-    memset(cnt , 0 , sizeof(cnt));//cnt记录后面n个位置放置的对应字符数量
+    memset(cnt , 0 , sizeof(cnt));
     int add1 = 0 , add2 = 0;
-    for(int j = 26 ; j >= x + 1 ; j --) add1 += sum[j];//1~pos-1里边比当前插入字符大的字符数量
+    for(int j = 26 ; j >= x + 1 ; j --) add1 += sum[j];
     sum[x] ++ ;//当前字符数量增加1
+    // 逐个位置去尝试。
     for(int L = 1 ; L <= n ; L ++){
-        //ma保存最大逆序对个数 L-1-cnt[j]+num
-        //L-1-cnt[j]是当前字符之后的比当前字符小的字符数量的最大值
-        //num是1~pos+L-1前的比当前放置字符字典序大的字符数量
+        // num表示的是：从1~i(这个i是主函数循环里的i)当中，能够和当前所用字母组成逆序对的字母个数
+        // 之所以要倒序循环，也是为了保证他们能组成逆序对
+        // 例如当前j到了字母x，那么num已经加过了sum[y] sum[z]，这些都是一定能产生逆序对的
+        // 而cnt[j]则表示在放置当前字母之前，i+1~len当中，已经有的j字母的个数
+        // 而从i+1~len，能产生的逆序对的最大数量，就是除j以外，其他字母的数量，也就是总数-已经有的-当前这个位置=L-cnt[j]-1
         int ma = -1 , pos = 0 , num = 0;
         for(int j = 26 ; j >= 1 ; j --){
             if(L - 1 - cnt[j] + num > ma){
@@ -23,6 +28,7 @@ bool check(int x , int n){
         }
         add2 += ma , cnt[pos] ++;
     }
+    // now表示已经确定的逆序对数量
     if(now + add1 + add2 >= V) {
         now += add1;
         return true;
