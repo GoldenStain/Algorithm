dc06e81bf93efae0085d2ea1759f8e2f77adbbba
你猜 <117290962+GoldenStain@users.noreply.github.com>
2025-03-17 15:13:52 +0800

    update solutions for leetcode 139.

--- leetcode 139.cpp
+++ leetcode 139.cpp
@@ -1,19 +1,78 @@
 // greedy solution(wrong)
 class Solution {
-    public:
-        bool wordBreak(string s, vector<string>& wordDict) {
-            std::unordered_set<std::string> S;
-            for (auto& si: wordDict) {
-                S.insert(si);
+   public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        std::unordered_set<std::string> S;
+        for (auto& si : wordDict) {
+            S.insert(si);
+        }
+        int hh = 0, tt = -1, len = s.size();
+        while (tt < len) {
+            tt++;
+            std::string tmp = s.substr(hh, tt - hh + 1);
+            if (S.find(tmp) != S.end()) {
+                hh = tt + 1;
             }
-            int hh = 0, tt = -1, len = s.size();
-            while(tt < len) {
-                tt++;
-                std::string tmp = s.substr(hh, tt - hh + 1);
-                if (S.find(tmp) != S.end()) {
-                    hh = tt + 1;
+        }
+        return (hh == len);
+    }
+};
+
+// DP solution
+class Solution {
+    using string = std::string;
+
+   public:
+    bool wordBreak(string s, std::vector<string> dicts) {
+        std::unordered_set<int> S;
+        std::unordered_set<string> dict_s;
+        for (auto& si : dicts) {
+            dict_s.insert(si);
+        }
+        int len = s.size();
+        // 开始的时候可以插入一个-1，这样在循环内部就不用写特判了。
+        S.insert(-1);
+        for (int i = 0; i < len; i++) {
+            for (const int& pos : S) {
+                string tmp = s.substr(pos + 1, i - pos);
+                if (dict_s.find(tmp) != dict_s.end()) {
+                    S.insert(i);
                 }
             }
-            return (hh == len);
         }
-    };
+        return S.find(len - 1) != S.end();
+    }
+};
+
+// faster DP
+class Solution {
+    using string = std::string;
+
+   public:
+    bool wordBreak(string s, std::vector<string> dicts) {
+        int len = s.size();
+        std::vector<int> dp(len+1, false);
+        // 这里有一个小技巧：
+        // 我们要保证能正常更新，我们必须要有一个为true的边界条件作为起点
+        // 但是如果我们令dp[0]=true,在s长度只有1的时候，如s="a", dicts={"b"}
+        // 这时候会误判
+        // 我们把dp的下标范围平移到[1, len]
+        // 每次更新的时候也就把下标+1，最后返回的答案就是dp[len]
+        // 这样在长度仅为1的时候就不会误判了。
+        dp[0] = true;
+        for (int i = 0; i < len; i++) {
+            // 没必要进行转移
+            if (!dp[i])
+                continue;
+            for (int j = 0; j < dicts.size(); j++) {
+                int jlen = dicts[j].size();
+                // 如果后面的元素个数不足jlen个，substr函数会自己截断。
+                string tmp =s.substr(i, jlen);
+                if (tmp == dicts[j]) {
+                    dp[i + jlen] = true;
+                }
+            }
+        }
+        return dp[len];
+    }
+};
--- test4.cpp
+++ test4.cpp
@@ -1,99 +1,63 @@
-#include <iostream>
 #include <stdio.h>
-#include <algorithm>
-#include <string>
-#include <cmath>
-#include <vector>
-#include <limits>
-#include <cstring>
-#include <string.h>
 
-#define For(i, j, n) for(int i = j ; i <= n ; ++i)
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <unordered_set>
+#include <vector>
+
+#define For(i, j, n) for (int i = j; i <= n; ++i)
 #ifdef DEBUG
 #define DEBUG_LOG(fmt, ...)                  \
-do {                  \
-\
-fprintf(stderr, fmt, ##__VA_ARGS__); \
-} while (0);
+    do {                                     \
+        fprintf(stderr, fmt, ##__VA_ARGS__); \
+    } while (0);
 #else
 #define DEBUG_LOG(fmt, ...) \
-do {                    \
-} while (0);
+    do {                    \
+    } while (0);
 #endif
 
 template <typename T>
-inline T read()
-{
+inline T read() {
     T x = 0;
-    int f = 1; char ch = getchar();
-    while(ch < '0' || ch > '9')
-    {
-        if(ch == '-') f = -1;
+    int f = 1;
+    char ch = getchar();
+    while (ch < '0' || ch > '9') {
+        if (ch == '-') f = -1;
         ch = getchar();
     }
-    while(ch >= '0' && ch <= '9')
-    {
-       x = x * 10 + ch - '0';
-       ch = getchar();
+    while (ch >= '0' && ch <= '9') {
+        x = x * 10 + ch - '0';
+        ch = getchar();
     }
     return x * f;
 }
 
-long long l, r;
+class Solution {
+    using string = std::string;
 
-long long dp[64][2][2][2][2];
-
-long long vl[64], vr[64];
-
-int cntl, cntr;
-
-void pre_fill(long long target, long long* v, int& cnt) {
-    if (!target) {
-        v[++cnt] = 0;
-        return;
+   public:
+    bool wordBreak(string s, std::vector<string> dicts) {
+        int len = s.size();
+        std::vector<int> dp(len+1, false);
+        dp[0] = true;
+        for (int i = 0; i < len; i++) {
+            // 没必要进行转移
+            if (!dp[i])
+                continue;
+            for (int j = 0; j < dicts.size(); j++) {
+                int jlen = dicts[j].size();
+                // 如果后面的元素个数不足jlen个，substr函数会自己截断。
+                string tmp =s.substr(i, jlen);
+                if (tmp == dicts[j]) {
+                    dp[i + jlen] = true;
+                }
+            }
+        }
+        return dp[len];
     }
-    while(target) {
-        v[++cnt] = target & 1;
-        target >>= 1;
-    }
-}
+};
 
-// dp[pos][][][][] 表示从pos位填到第一位，最值是多少
-// limit1表示贴合下界，limit2表示贴合上界
-long long get_dp(int pos, bool limitl1, bool limitr1, bool limitl2, bool limitr2) {
-    if (pos == 0) {
-        return 0ll;
-    }
-    long long ans = 0ll;
-    long long &d = dp[pos][limitl1][limitr1][limitl2][limitr2];
-    // 这里把limit也放进了状态表示当中，所以只要已经算出来了，就能直接返回。
-    if (d != -1) {
-        return d;
-    }
-    // 异或有两个操作数，我们分别选取
-    // 两个数字都要满足：l<=x<=r
-    for (long long u = (limitl1?vl[pos]:0); u <= (limitr1?vr[pos]:1); u++)
-        for (long long v = (limitl2?vl[pos]:0); v <= (limitr2?vr[pos]:1); v++)
-            ans = std::max(ans, ((u ^ v) << (pos - 1)) + get_dp(pos - 1, limitl1&(u == vl[pos]), limitr1&(u==vr[pos]), limitl2&(v==vl[pos]), limitr2&(v==vr[pos])));
-    d = ans;
-    return ans;
-}
-
-long long work(long long l, long long r) {
-    if (l == r) {
-        return 0ll;
-    }
-    memset(dp, -1, sizeof(dp));
-    pre_fill(l, vl, cntl);
-    pre_fill(r, vr, cntr);
-    return get_dp(std::max(cntl, cntr), 1, 1, 1, 1);// 一开始什么都没填，相当于所有限制都要满足，即第一次填的数字不能随便选
-}
-
-int main()
-{
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(0);
-    std::cin >> l >> r;
-    std::cout << work(l, r);
-    return 0;
-}
+int main() { return 0; }
