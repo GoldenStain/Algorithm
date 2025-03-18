68b69cc66dc6209e77d8f82aefeb182d1c002009
你猜 <117290962+GoldenStain@users.noreply.github.com>
2025-03-18 17:06:34 +0800

    add leetcode 322.

--- test4.cpp
+++ test4.cpp
@@ -34,41 +34,20 @@ inline T read() {
     return x * f;
 }
 
-using std::cin, std::cout;
 using std::vector;
+
 class Solution {
    public:
-    int maximalRectangle(vector<vector<char>>& matrix) {
-        int m = matrix.size(), n = matrix[0].size();
-        vector<vector<int>> ones(m, vector<int>(n, 0));
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == '1') {
-                    ones[i][j] = (j==0?0:ones[i][j - 1]) + 1;
-                }
-            }
-        }// 预处理完毕
-        std::vector<int> stk, up, down;
-        int ans = 0;
-        for (int j = 0; j < n; j++) {
-            stk.clear();
-            up.assign(m, -1);
-            down.assign(m, m);
-            for (int i = 0; i < m; i++) {
-                while (stk.size() && ones[i][j] < ones[stk.back()][j]) {
-                    down[stk.back()] = i;
-                    stk.pop_back();
-                }
-                if (stk.size())
-                    up[i] = stk.back();
-                stk.push_back(i);
-            }
-            for (int i = 0; i < m; i++) {
-                int width = down[i] - up[i] - 1;
-                ans = std::max(ans, width * ones[i][j]);
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        vector<int> dp(amount + 1, 1e5);
+        dp[0] = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = coins[i]; j <= amount; j++) {
+                dp[j] = std::min(dp[j - coins[i]] + 1, dp[j]);
             }
         }
-        return ans;
+        return dp[amount]==1e5?-1:dp[amount];
     }
 };
 
