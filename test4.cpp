609d6d876deae7528909d6e6f9ca1e3a2b0bf430
你猜 <117290962+GoldenStain@users.noreply.github.com>
2025-03-26 20:25:24 +0800

    add leetcode 103.

--- test4.cpp
+++ test4.cpp
@@ -3,6 +3,7 @@
 
 #include <algorithm>
 #include <cmath>
+#include <deque>
 #include <iostream>
 #include <memory>
 #include <queue>
@@ -58,27 +59,28 @@ class Solution {
         if (!root) {
             return vector<vector<int>>{};
         }
+        std::queue<TreeNode *> q;
         vector<vector<int>> ans;
-        dfs(root, 1, ans);
+        q.push(root);
+        int depth = 1;
+        while (q.size()) {
+            int n = q.size();
+            std::deque<int> tmp;
+            for (int i = 0; i < n; i++) {
+                auto head = q.front();
+                q.pop();
+                if (depth & 1) {
+                    tmp.push_back(head->val);
+                } else {
+                    tmp.push_front(head->val);
+                }
+                if (head->left) q.push(head->left);
+                if (head->right) q.push(head->right);
+            }
+            ans.emplace_back(vector<int>(tmp.begin(), tmp.end()));
+            depth++;
+        }
         return ans;
-    }
-    void dfs(TreeNode *root, int depth, vector<vector<int>> &ans) {
-        if (!root) {
-            return;
-        }
-        if (ans.size() < depth) {
-            ans.push_back(vector<int>{});
-        }
-        ans[depth - 1].push_back(root->val);
-        // 当前是偶数，那么下一层是奇数，注意我们在这一层就能决定下一层的访问顺序，所以我们真正考虑的是下一层的奇偶性。
-        if ((depth & 1) == 0) {
-            dfs(root->left, depth + 1, ans);
-            dfs(root->right, depth + 1, ans);
-        }
-        else {
-            dfs(root->right, depth + 1, ans);
-            dfs(root->left, depth + 1, ans);
-        }
     }
 };
 
