b5ba2c3d79b21c5c957e4cfae87ffd791cd8aae0
你猜 <117290962+GoldenStain@users.noreply.github.com>
2025-03-30 01:07:49 +0800

    add code for leetcode 1703.

--- leetcode 1703.cpp
+++ leetcode 1703.cpp
@@ -1,59 +1,3 @@
-#include <stdio.h>
-#include <string.h>
-
-#include <algorithm>
-#include <cmath>
-#include <deque>
-#include <iostream>
-#include <memory>
-#include <queue>
-#include <string>
-#include <unordered_set>
-#include <vector>
-
-#define For(i, j, n) for (int i = j; i <= n; ++i)
-#ifdef DEBUG
-#define DEBUG_LOG(fmt, ...)                  \
-    do {                                     \
-        fprintf(stderr, fmt, ##__VA_ARGS__); \
-    } while (0);
-#else
-#define DEBUG_LOG(fmt, ...) \
-    do {                    \
-    } while (0);
-#endif
-
-template <typename T>
-inline T read() {
-    T x = 0;
-    int f = 1;
-    char ch = getchar();
-    while (ch < '0' || ch > '9') {
-        if (ch == '-') f = -1;
-        ch = getchar();
-    }
-    while (ch >= '0' && ch <= '9') {
-        x = x * 10 + ch - '0';
-        ch = getchar();
-    }
-    return x * f;
-}
-
-struct TreeNode {
-    int val;
-    TreeNode* left;
-    TreeNode* right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode* left, TreeNode* right)
-        : val(x), left(left), right(right) {}
-};
-
-using std::queue;
-using std::shared_ptr;
-using std::string;
-using std::vector;
-
 class Solution {
    public:
     int minMoves(vector<int>& nums, int k) {
@@ -97,9 +41,3 @@ class Solution {
             return std::min(a, b);
     }
 };
-
-int main() {
-    string s;
-    std::cout << s.size() << std::endl;
-    return 0;
-}
