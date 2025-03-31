dc981a4ea9140f2128a7929024893f8c44f13c5f
你猜 <117290962+GoldenStain@users.noreply.github.com>
2025-03-31 19:14:57 +0800

    add leetcode 82.

--- leetcode 82. 删除排序链表中的重复元素 II.cpp
+++ leetcode 82. 删除排序链表中的重复元素 II.cpp
@@ -10,105 +10,102 @@
  */
 
 // solution
- class Solution {
-    public:
-        ListNode* deleteDuplicates(ListNode* head) {
-            ListNode *dummy = new ListNode(-101, nullptr);
-            int cnt = 0;
-            ListNode *now = dummy, *prev = dummy;
-            while(head) {
-                if (head->val == prev->val)
-                    cnt++;
-                else {
-                    if (cnt == 1) {
-                        now->next = new ListNode(prev->val, nullptr);
-                        now = now->next;
-                    }
-                    cnt = 1;
+class Solution {
+   public:
+    ListNode *deleteDuplicates(ListNode *head) {
+        ListNode *dummy = new ListNode(-101, nullptr);
+        int cnt = 0;
+        ListNode *now = dummy, *prev = dummy;
+        while (head) {
+            if (head->val == prev->val)
+                cnt++;
+            else {
+                if (cnt == 1) {
+                    now->next = new ListNode(prev->val, nullptr);
+                    now = now->next;
                 }
-                prev = head;
-                head =  head->next;
+                cnt = 1;
             }
-            if (cnt == 1)
-                now->next = new ListNode(prev->val, nullptr);
-            return dummy->next;
+            prev = head;
+            head = head->next;
         }
-    };
+        if (cnt == 1) now->next = new ListNode(prev->val, nullptr);
+        return dummy->next;
+    }
+};
 
 // wrong code
 class Solution {
-    public:
-        ListNode* deleteDuplicates(ListNode* head) {
-            ListNode *dummy = new ListNode(-101, head);
-            int cnt = 0;
-            ListNode *now = dummy, *prev = dummy;
-            while(head) {
-                if (head->val == prev->val)
-                    cnt++;
-                else {
-                    if (cnt == 1) {
-                        now->next = prev;
-                        now = now->next;
-                    }
-                    cnt = 1;
+   public:
+    ListNode *deleteDuplicates(ListNode *head) {
+        ListNode *dummy = new ListNode(-101, head);
+        int cnt = 0;
+        ListNode *now = dummy, *prev = dummy;
+        while (head) {
+            if (head->val == prev->val)
+                cnt++;
+            else {
+                if (cnt == 1) {
+                    now->next = prev;
+                    now = now->next;
                 }
-                prev = head;
-                head =  head->next;
+                cnt = 1;
             }
-            if (cnt == 1)
-                now->next = prev;
-            return dummy->next;
+            prev = head;
+            head = head->next;
         }
-    };
-
+        if (cnt == 1) now->next = prev;
+        return dummy->next;
+    }
+};
 
 // improved wrong code, which is right
 class Solution {
-    public:
-        ListNode* deleteDuplicates(ListNode* head) {
-            ListNode *dummy = new ListNode(-101, head);
-            int cnt = 0;
-            ListNode *now = dummy, *prev = dummy;
-            while(head) {
-                if (head->val == prev->val)
-                    cnt++;
-                else {
-                    if (cnt == 1) {
-                        now->next = prev;
-                        now = now->next;
-                    }
-                    cnt = 1;
+   public:
+    ListNode *deleteDuplicates(ListNode *head) {
+        ListNode *dummy = new ListNode(-101, head);
+        int cnt = 0;
+        ListNode *now = dummy, *prev = dummy;
+        while (head) {
+            if (head->val == prev->val)
+                cnt++;
+            else {
+                if (cnt == 1) {
+                    now->next = prev;
+                    now = now->next;
                 }
-                prev = head;
-                head =  head->next;
+                cnt = 1;
             }
-            if (cnt == 1)
-                now->next = prev, now = now->next;
-            now->next = nullptr;
-            return dummy->next;
+            prev = head;
+            head = head->next;
         }
-    };
+        if (cnt == 1) now->next = prev, now = now->next;
+        now->next = nullptr;
+        return dummy->next;
+    }
+};
 
 // improved official solution with delete operation
 class Solution {
-    public:
-        ListNode* deleteDuplicates(ListNode* head) {
-            ListNode *dummy = new ListNode(-101, head); ListNode *cur = dummy;
-            // 后面不到两个就不用判断了
-            while(cur->next &&cur->next->next) {
-                if (cur->next->val == cur->next->next->val) {
-                    int x = cur->next->val;
-                    while(cur->next && cur->next->val == x) {
-                        ListNode *l = cur->next;
-                        cur->next = cur->next->next;
-                        delete l;
-                    }
+   public:
+    ListNode *deleteDuplicates(ListNode *head) {
+        ListNode *dummy = new ListNode(-101, head);
+        ListNode *cur = dummy;
+        // 后面不到两个就不用判断了
+        while (cur->next && cur->next->next) {
+            if (cur->next->val == cur->next->next->val) {
+                int x = cur->next->val;
+                while (cur->next && cur->next->val == x) {
+                    ListNode *l = cur->next;
+                    cur->next = cur->next->next;
+                    delete l;
                 }
-                else cur = cur->next;
-            }
-            return dummy->next;
+            } else
+                cur = cur->next;
         }
-    };
+        return dummy->next;
+    }
+};
 
 // improved my solution
 // 像支持删除就得把我的思路从向前看改成向后看，改成向后看的话，就跟官方题解一模一样了
