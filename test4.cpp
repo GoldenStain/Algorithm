6c209ff7dc208e1a05da523ab39965aaccc47678
你猜 <117290962+GoldenStain@users.noreply.github.com>
2025-04-01 21:10:57 +0800

    add a new solution for leetcode 470.

--- test4.cpp
+++ test4.cpp
@@ -68,6 +68,21 @@ class Solution {
             if (res < 40) {
                 return res / 4 + 1;
             }
+            // a -> 9
+            b = rand7();
+            a = res - 40;
+            res = a * 7 + b - 1;
+            if (res < 60) {
+                return res / 6 + 1;
+            }
+            // a -> 3
+            b = rand7();
+            a = res - 60;
+            res = a * 7 + b - 1;
+            if (res < 20) {
+                return res / 2 + 1;
+            }
+            // a -> 1，无法和b组合出rand10了，进入新一轮循环。
         }
     }
 };
