f85164bcd6432fd2b86ded83478db7138e76e2f0
goldenstain <xym@MSI>
2025-04-10 17:27:32 +0800

    updated code for leetcode 114. solution 1

--- leetcode 114.cpp
+++ leetcode 114.cpp
@@ -1,38 +1,44 @@
 // 树上翻转链表
 
 class Solution {
-   public:
-    TreeNode* preNode = nullptr;
-    void flatten(TreeNode* root) {
-        if (!root) return;
-        flatten(root->right);
-        flatten(root->left);
-        // 要记得把root->left设置为nullptr.
-        root->left = nullptr;
-        root->right = preNode;
-        preNode = root;
-    }
+public:
+  // lastNode是在回溯过程中，经过的上一个节点。
+  TreeNode *lastNode = nullptr;
+  void flatten(TreeNode *root) {
+    if (!root)
+      return;
+    flatten(root->right);
+    flatten(root->left);
+    // 不需要显式地链接左右子树，因为在flatten(root->left)的时候，在子树最底部的节点，root->right
+    // = lastNode已经完成了这个任务
+    root->left = nullptr;
+    root->right = lastNode;
+    lastNode = root;
+  }
 };
 
 // 空间复杂度O(1)
 
 class Solution {
-   public:
-    void flatten(TreeNode* root) {
-        if (!root) return;
-        TreeNode* cur = root;
-        while (cur) {
-            if (cur->left) {
-                TreeNode* next = cur->left;
-                TreeNode* p = next;
-                while (p->right) p = p->right;
-                if (p) p->right = cur->right;
-                // 同样要记得断开左边的链接
-                cur->left = nullptr;
-                cur->right = next;
-            }
+public:
+  void flatten(TreeNode *root) {
+    if (!root)
+      return;
+    TreeNode *cur = root;
+    while (cur) {
+      if (cur->left) {
+        TreeNode *next = cur->left;
+        TreeNode *p = next;
+        while (p->right)
+          p = p->right;
+        if (p)
+          p->right = cur->right;
+        // 同样要记得断开左边的链接
+        cur->left = nullptr;
+        cur->right = next;
+      }
 
-            cur = cur->right;
-        }
+      cur = cur->right;
     }
+  }
 };
--- test4.cpp
+++ test4.cpp
@@ -65,46 +65,17 @@ using std::vector;
 
 class Solution {
 public:
-  ListNode *reverseKGroup(ListNode *head, int k) {
-    if (!head || !head->next)
-      return head;
-    int len = getLength(head);
-    ListNode *dummy = new ListNode(-1, head);
-    ListNode *precedor = dummy;
-    int pos = 0;
-    // 不包括pos自己，所以不用-1
-    while (pos + k <= len) {
-      oneReverse(precedor, 1, k);
-      // 移动当前位置
-      pos += k;
-    }
-    return dummy->next;
-  }
-  int getLength(ListNode *head) {
-    int cnt = 0;
-    while (head) {
-      cnt++;
-      head = head->next;
-    }
-    return cnt;
-  }
-  void oneReverse(ListNode *&precedor, int left, int right) {
-    ListNode *cur = precedor->next, *prev = precedor;
-    for (int cnt = left; cnt <= right; cnt++) {
-      ListNode *tmp_next = cur->next;
-      if (cnt > left) {
-        cur->next = prev;
-      }
-      prev = cur;
-      cur = tmp_next;
-    }
-    // 原来的第一个变成了最后一个，也是下一段的precedor
-    ListNode *new_precedor = precedor->next;
-    new_precedor->next = cur;
-    // 最后一个变成第一个
-    precedor->next = prev;
-    // 更新precedor
-    precedor = new_precedor;
+  // lastNode是在回溯过程中，经过的上一个节点。
+  TreeNode *lastNode = nullptr;
+  void flatten(TreeNode *root) {
+    if (!root)
+      return;
+    flatten(root->right);
+    flatten(root->left);
+    // 不需要显式地链接左右子树，因为在flatten(root->left)的时候，在子树最底部的节点，root->right = lastNode已经完成了这个任务
+    root->left = nullptr;
+    root->right = lastNode;
+    lastNode = root;
   }
 };
 
