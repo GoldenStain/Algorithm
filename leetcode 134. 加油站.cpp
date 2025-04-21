a7258a520a78887fa4cb0ef14cf001c3f537ee19
GoldenStain <goldenstainxym@gmail.com>
2025-04-21 11:28:36 +0800

    add a better solution for leetcode 134. with a new picture

--- leetcode 134. 加油站.cpp
+++ leetcode 134. 加油站.cpp
@@ -1,22 +1,39 @@
 class Solution {
-    public:
-        int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-            int start = 0, n = gas.size();
-            while(start < n) {
-                int sumGas = 0, sumCost = 0, cnt = 0;
-                while(cnt < n) {
-                    int j = (start + cnt) % n;
-                    sumGas += gas[j];
-                    sumCost += cost[j];
-                    if (sumCost > sumGas) {
-                        break;
-                    }
-                    cnt++;
-                }
-                if (cnt == n)
-                    return start;
-                start = start + cnt + 1;
-            }
-            return -1;
+public:
+  int canCompleteCircuit(vector<int> &gas, vector<int> &cost) {
+    int start = 0, n = gas.size();
+    while (start < n) {
+      int sumGas = 0, sumCost = 0, cnt = 0;
+      while (cnt < n) {
+        int j = (start + cnt) % n;
+        sumGas += gas[j];
+        sumCost += cost[j];
+        if (sumCost > sumGas) {
+          break;
         }
-    };
+        cnt++;
+      }
+      if (cnt == n)
+        return start;
+      start = start + cnt + 1;
+    }
+    return -1;
+  }
+};
+
+// Better solution
+
+class Solution {
+public:
+  int canCompleteCircuit(vector<int> &gas, vector<int> &cost) {
+    int n = gas.size(), minPos = -1, minValue = INT_MAX, sum = 0;
+    for (int i = 0; i < n; i++) {
+      sum += gas[i] - cost[i];
+      if (sum < minValue) {
+        minValue = sum;
+        minPos = i;
+      }
+    }
+    return (sum < 0) ? -1 : (minPos + 1) % n;
+  }
+};
--- test2.cpp
+++ test2.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <climits>
 #include <cmath>
 #include <cstddef>
 #include <deque>
@@ -69,86 +70,16 @@ using std::vector;
 
 class Solution {
 public:
-  ListNode *sortList(ListNode *head) {
-    if (!head || !head->next) {
-      return head;
-    }
-    ListNode *dummy = new ListNode(-1, head);
-    int totalLength = getLength(head);
-    // 每次都合并两条短的，把它们变有序，得到更长的有序序列
-    // 比交换节点更安全，也更好写
-    for (int curLength = 1; curLength < totalLength; curLength <<= 1) {
-      // 排序之后head的位置可能发生变化，所以这里不能用head，要用dummy->next
-      ListNode *cur = dummy->next, *leftBound = dummy;
-      // 不需要用一个int记录cur的位置
-      // 因为这道题不同于K个一组翻转链表
-      // 最后长度不及K个就不操作了
-      // 这道题最后长度不够的部分也要排序
-      // 所以cur一直移动到最后就行
-      while (cur) {
-        ListNode *firstList = cur;
-        // 这里不需要 && cur，可以直接&& cur->next
-        for (int i = 1; i < curLength && cur->next; i++) {
-          cur = cur->next;
-        }
-        ListNode *nextCur = cur->next, *secondList = nextCur;
-        cur->next =
-            nullptr; // 合并链表的时候，传入的链表需要末尾是nullptr，所以这里我们手动把链表断开
-        cur = nextCur;
-        for (int i = 1; i < curLength && cur && cur->next; i++) {
-          cur = cur->next;
-        }
-        // 这里的nextCur已经被用过，先前的值是一个“脏值”，我们要先给它复原
-        // 也可以前面不定义nextCur，到了这一步再定义，并赋初值
-        nextCur = nullptr;
-        // 这里有可能cur已经移动到nullptr了，所以需要用if防止越界
-        // 讲课：把这个if去掉，观察现象
-        if (cur) {
-          nextCur = cur->next;
-          cur->next = nullptr;
-        }
-        // 这里第一个参数不能用leftBound->next,这时候链表还没连起来，必须用firstList
-        leftBound->next = mergeLists(firstList, secondList);
-        // 因为排序之后cur的位置变了，我们不能用leftBound =
-        // cur的方式来移动leftBound,只能通过循环，把leftBound一步一步地往后移
-        while (leftBound->next) {
-          leftBound = leftBound->next;
-        }
-        // 我们前面通过cur->next = nullptr把链表断开了，这里为什么不用恢复呢？
-        // 分析一下能够发现，leftBound->next = mergeLists(leftBound->next,
-        // secondList); 不仅进行了排序，也完成了恢复工作
-        // 每次leftBound的位置，都是在待排序位置的前一个，相当于一个动态的dummy
-        cur = nextCur;
+  int canCompleteCircuit(vector<int> &gas, vector<int> &cost) {
+    int n = gas.size(), minPos = -1, minValue = INT_MAX, sum = 0;
+    for (int i = 0; i < n; i++) {
+      sum += gas[i] - cost[i];
+      if (sum < minValue) {
+        minValue = sum;
+        minPos = i;
       }
     }
-    return dummy->next;
-  }
-  inline int getLength(ListNode *head) {
-    int cnt = 0;
-    while (head) {
-      cnt++;
-      head = head->next;
-    }
-    return cnt;
-  }
-
-  inline ListNode *mergeLists(ListNode *l1, ListNode *l2) {
-    ListNode *dummy = new ListNode(-1), *returnedDummy = dummy;
-    while (l1 && l2) {
-      if (l1->val <= l2->val) {
-        dummy->next = l1;
-        l1 = l1->next;
-      } else {
-        dummy->next = l2;
-        l2 = l2->next;
-      }
-      dummy = dummy->next;
-    }
-    if (l1)
-      dummy->next = l1;
-    if (l2)
-      dummy->next = l2;
-    return returnedDummy->next;
+    return (sum < 0) ? -1 : (minPos + 1) % n;
   }
 };
 
