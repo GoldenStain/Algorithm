2a83dea639267e03f87f5aa808b7af764b2749e2
GoldenStain <goldenstainxym@gmail.com>
2025-04-23 16:04:48 +0800

    re-formatted some codes

--- leetcode 140.cpp
+++ leetcode 140.cpp
@@ -1,35 +1,35 @@
 class Solution {
-    std::unordered_set<std::string> S;
-    vector<bool> vis;
+  std::unordered_set<std::string> S;
+  vector<bool> vis;
 
-   public:
-    vector<string> wordBreak(string s, vector<string>& wordDict) {
-        S = std::unordered_set<std::string>(wordDict.begin(), wordDict.end());
-        std::vector<std::vector<string>> splited_words(s.size() + 1,
-                                                       vector<string>{});
-        vis = std::vector<bool>(s.size() + 1, false);
-        dfs(0, splited_words, s);
-        return splited_words[0];
+public:
+  vector<string> wordBreak(string s, vector<string> &wordDict) {
+    S = std::unordered_set<std::string>(wordDict.begin(), wordDict.end());
+    std::vector<std::vector<string>> splited_words(s.size() + 1,
+                                                   vector<string>{});
+    vis = std::vector<bool>(s.size() + 1, false);
+    dfs(0, splited_words, s);
+    return splited_words[0];
+  }
+  void dfs(int pos, std::vector<std::vector<string>> &splited_words,
+           string &s) {
+    if (vis[pos])
+      return;
+    vis[pos] = true;
+    if (pos == s.size()) {
+      // 需要手动放一个空的字符串，否则不能跟前面的接上
+      splited_words[pos] = {""};
+      return;
     }
-    void dfs(int pos, std::vector<std::vector<string>>& splited_words,
-             string& s) {
-        if (vis[pos]) return;
-        vis[pos] = true;
-        if (pos == s.size()) {
-            // 需要手动放一个空的字符串，否则不能跟前面的接上
-            splited_words[pos] = {""};
-            return;
+    for (int i = pos + 1; i <= s.size(); i++) {
+      // pos ~ i-1 是当前的，i~往后是后面一串的
+      string tmp = s.substr(pos, i - pos);
+      dfs(i, splited_words, s);
+      if (S.count(tmp)) {
+        for (auto &si : splited_words[i]) {
+          splited_words[pos].emplace_back(si.empty() ? tmp : tmp + " " + si);
         }
-        for (int i = pos + 1; i <= s.size(); i++) {
-            // pos ~ i-1 是当前的，i~往后是后面一串的
-            string tmp = s.substr(pos, i - pos);
-            dfs(i, splited_words, s);
-            if (S.count(tmp)) {
-                for (auto& si : splited_words[i]) {
-                    splited_words[pos].emplace_back(si.empty() ? tmp
-                                                               : tmp + " " + si);
-                }
-            }
-        }
+      }
     }
+  }
 };
--- leetcode 82. 删除排序链表中的重复元素 II.cpp
+++ leetcode 82. 删除排序链表中的重复元素 II.cpp
@@ -11,100 +11,103 @@
 
 // solution
 class Solution {
-   public:
-    ListNode *deleteDuplicates(ListNode *head) {
-        ListNode *dummy = new ListNode(-101, nullptr);
-        int cnt = 0;
-        ListNode *now = dummy, *prev = dummy;
-        while (head) {
-            if (head->val == prev->val)
-                cnt++;
-            else {
-                if (cnt == 1) {
-                    now->next = new ListNode(prev->val, nullptr);
-                    now = now->next;
-                }
-                cnt = 1;
-            }
-            prev = head;
-            head = head->next;
+public:
+  ListNode *deleteDuplicates(ListNode *head) {
+    ListNode *dummy = new ListNode(-101, nullptr);
+    int cnt = 0;
+    ListNode *now = dummy, *prev = dummy;
+    while (head) {
+      if (head->val == prev->val)
+        cnt++;
+      else {
+        if (cnt == 1) {
+          now->next = new ListNode(prev->val, nullptr);
+          now = now->next;
         }
-        if (cnt == 1) now->next = new ListNode(prev->val, nullptr);
-        return dummy->next;
+        cnt = 1;
+      }
+      prev = head;
+      head = head->next;
     }
+    if (cnt == 1)
+      now->next = new ListNode(prev->val, nullptr);
+    return dummy->next;
+  }
 };
 
 // wrong code
 class Solution {
-   public:
-    ListNode *deleteDuplicates(ListNode *head) {
-        ListNode *dummy = new ListNode(-101, head);
-        int cnt = 0;
-        ListNode *now = dummy, *prev = dummy;
-        while (head) {
-            if (head->val == prev->val)
-                cnt++;
-            else {
-                if (cnt == 1) {
-                    now->next = prev;
-                    now = now->next;
-                }
-                cnt = 1;
-            }
-            prev = head;
-            head = head->next;
+public:
+  ListNode *deleteDuplicates(ListNode *head) {
+    ListNode *dummy = new ListNode(-101, head);
+    int cnt = 0;
+    ListNode *now = dummy, *prev = dummy;
+    while (head) {
+      if (head->val == prev->val)
+        cnt++;
+      else {
+        if (cnt == 1) {
+          now->next = prev;
+          now = now->next;
         }
-        if (cnt == 1) now->next = prev;
-        return dummy->next;
+        cnt = 1;
+      }
+      prev = head;
+      head = head->next;
     }
+    if (cnt == 1)
+      now->next = prev;
+    return dummy->next;
+  }
 };
 
 // improved wrong code, which is right
 class Solution {
-   public:
-    ListNode *deleteDuplicates(ListNode *head) {
-        ListNode *dummy = new ListNode(-101, head);
-        int cnt = 0;
-        ListNode *now = dummy, *prev = dummy;
-        while (head) {
-            if (head->val == prev->val)
-                cnt++;
-            else {
-                if (cnt == 1) {
-                    now->next = prev;
-                    now = now->next;
-                }
-                cnt = 1;
-            }
-            prev = head;
-            head = head->next;
+public:
+  ListNode *deleteDuplicates(ListNode *head) {
+    ListNode *dummy = new ListNode(-101, head);
+    int cnt = 0;
+    ListNode *now = dummy, *prev = dummy;
+    while (head) {
+      if (head->val == prev->val)
+        cnt++;
+      else {
+        if (cnt == 1) {
+          now->next = prev;
+          now = now->next;
         }
-        if (cnt == 1) now->next = prev, now = now->next;
-        now->next = nullptr;
-        return dummy->next;
+        cnt = 1;
+      }
+      prev = head;
+      head = head->next;
     }
+    if (cnt == 1)
+      now->next = prev, now = now->next;
+    now->next = nullptr;
+    return dummy->next;
+  }
 };
 
 // improved official solution with delete operation
 class Solution {
-   public:
-    ListNode *deleteDuplicates(ListNode *head) {
-        ListNode *dummy = new ListNode(-101, head);
-        ListNode *cur = dummy;
-        // 后面不到两个就不用判断了
-        while (cur->next && cur->next->next) {
-            if (cur->next->val == cur->next->next->val) {
-                int x = cur->next->val;
-                while (cur->next && cur->next->val == x) {
-                    ListNode *l = cur->next;
-                    cur->next = cur->next->next;
-                    delete l;
-                }
-            } else
-                cur = cur->next;
+public:
+  ListNode *deleteDuplicates(ListNode *head) {
+    ListNode *dummy = new ListNode(-101, head);
+    ListNode *cur = dummy;
+    // 后面不到两个就不用判断了
+    while (cur->next && cur->next->next) {
+      if (cur->next->val == cur->next->next->val) {
+        int x = cur->next->val;
+        while (cur->next && cur->next->val == x) {
+          ListNode *l = cur->next;
+          cur->next = cur->next->next;
+          delete l;
         }
-        return dummy->next;
+      } else
+        cur = cur->next;
     }
+    return dummy->next;
+  }
 };
 
 // improved my solution
