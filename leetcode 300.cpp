91141a0c21f06618a6b36bca6c882a7f8a71cacc
GoldenStain <goldenstainxym@gmail.com>
2025-04-23 23:01:53 +0800

    re-formatted code for leetcode 300.

--- leetcode 300.cpp
+++ leetcode 300.cpp
@@ -1,21 +1,21 @@
 class Solution {
-   public:
-    int lengthOfLIS(vector<int> &nums) {
-        if (nums.size() == 1) {
-            return 1;
-        }
-        int n = nums.size();
-        vector<int> ups;
-        ups.push_back(nums[0]);
-        for (int i = 1; i < n; i++) {
-            if (nums[i] > ups.back()) {
-                ups.emplace_back(nums[i]);
-            } else {
-                auto pos = std::lower_bound(ups.begin(), ups.end(), nums[i]);
-                std::cout << pos - ups.begin();
-                *pos = nums[i];
-            }
-        }
-        return ups.size();
+public:
+  int lengthOfLIS(vector<int> &nums) {
+    if (nums.size() == 1) {
+      return 1;
     }
+    int n = nums.size();
+    vector<int> ups;
+    ups.push_back(nums[0]);
+    for (int i = 1; i < n; i++) {
+      if (nums[i] > ups.back()) {
+        ups.emplace_back(nums[i]);
+      } else {
+        auto pos = std::lower_bound(ups.begin(), ups.end(), nums[i]);
+        std::cout << pos - ups.begin();
+        *pos = nums[i];
+      }
+    }
+    return ups.size();
+  }
 };
