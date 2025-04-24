ccc36d29c7395bdf86a02c9ee9a992ae2b19b73d
GoldenStain <goldenstainxym@gmail.com>
2025-04-24 23:26:40 +0800

    re-formatted code for leetcode 76.

--- leetcode 76. 最小覆盖子串.cpp
+++ leetcode 76. 最小覆盖子串.cpp
@@ -1,35 +1,36 @@
 class Solution {
-    public:
-        string minWindow(string s, string t) {
-            std::unordered_map<char, int> M;
-            std::unordered_set<char> S, MS;
-            std::string ans = "";
-            int ans_len = 1e6;
-            for(int i = 0; i < t.size(); i++) {
-                S.insert(t[i]);
-                M[t[i]]--;
-            }
-            int hh = 0, tt = 0;
-            for(;tt < s.size(); tt++) {
-                if (S.count(s[tt])) {
-                    M[s[tt]]++;
-                    if (M[s[tt]] >= 0)
-                        MS.insert(s[tt]);
-                }
-                if (MS.size() == S.size()) {
-                    // update
-                    while(hh <= tt && (!S.count(s[hh]) || M[s[hh]] > 0)) {
-                        if (S.count(s[hh])) M[s[hh]]--;
-                        hh++;
-                    }
-                    // check
-                    int len = tt - hh + 1;
-                    if (ans_len > len) {
-                        ans_len = len;
-                        ans = s.substr(hh, len);
-                    }
-                }
-            }
-            return ans;
+public:
+  string minWindow(string s, string t) {
+    std::unordered_map<char, int> M;
+    std::unordered_set<char> S, MS;
+    std::string ans = "";
+    int ans_len = 1e6;
+    for (int i = 0; i < t.size(); i++) {
+      S.insert(t[i]);
+      M[t[i]]--;
+    }
+    int hh = 0, tt = 0;
+    for (; tt < s.size(); tt++) {
+      if (S.count(s[tt])) {
+        M[s[tt]]++;
+        if (M[s[tt]] >= 0)
+          MS.insert(s[tt]);
+      }
+      if (MS.size() == S.size()) {
+        // update
+        while (hh <= tt && (!S.count(s[hh]) || M[s[hh]] > 0)) {
+          if (S.count(s[hh]))
+            M[s[hh]]--;
+          hh++;
         }
-    };
+        // check
+        int len = tt - hh + 1;
+        if (ans_len > len) {
+          ans_len = len;
+          ans = s.substr(hh, len);
+        }
+      }
+    }
+    return ans;
+  }
+};
