d35475bc064ba400eb25cdf0451d4862606e6e10
GoldenStain <goldenstainxym@gmail.com>
2025-04-30 00:51:12 +0800

    add best solution for leetcode 84.cpp

--- leetcode 84.cpp
+++ leetcode 84.cpp
@@ -47,3 +47,29 @@ public:
     return ans;
   }
 };
+
+// best solution
+class Solution {
+public:
+  int largestRectangleArea(vector<int> &heights) {
+    heights.insert(heights.begin(), 0);
+    heights.push_back(
+        0); // 放两个padding的值，等价于原先Solution当中，把left和right数组初始化为非法值
+    std::vector<int> stk;
+    int n = heights.size(), ans = 0;
+    for (int i = 0; i < n; i++) {
+      while (!stk.empty() && heights[stk.back()] > heights[i]) {
+        int now_height = heights[stk.back()];
+        stk.pop_back();
+        // 此时的栈顶是：now_height左边第一个比它小的
+        // 不用担心进行一次出栈操作后，栈上就为空了，
+        // 因为我们进行了padding操作，这里是一定有0来垫底的
+        int _left = stk.back() + 1, _right = i - 1;
+        // 两个比now_height更小的柱子中间，所夹住的部分，就是now_height所构成的合法矩形
+        ans = std::max(ans, (_right - _left + 1) * now_height);
+      }
+      stk.push_back(i);
+    }
+    return ans;
+  }
+};
