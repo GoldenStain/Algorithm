db64df11626cbd0fd8372bcc20aed15350f4e6c5
GoldenStain <goldenstainxym@gmail.com>
2025-06-08 02:23:11 +0800

    add iterative solution for leetcode 386.

--- leetcode 386.cpp
+++ leetcode 386.cpp
@@ -17,3 +17,23 @@ public:
     return ans;
   }
 };
+
+// iterative solution
+class Solution {
+public:
+  vector<int> lexicalOrder(int n) {
+    std::vector<int> ans;
+    for (int i = 0, j = 1; i < n; i++) {
+      ans.emplace_back(j);
+      if (j * 10 <= n)
+        j *= 10; // 保持字典序最小的方法就是往后加零
+      else {
+        while (j + 1 > n || j % 10 == 9) { // 手动模拟回溯过程
+          j /= 10;
+        }
+        j++;
+      }
+    }
+    return ans;
+  }
+};
