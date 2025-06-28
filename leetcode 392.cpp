0c200668c105218b09dc60f77f74c301a1d26a72
GoldenStain <goldenstainxym@gmail.com>
2025-06-29 02:55:48 +0800

    add advanced solution for leetcode 392.

--- leetcode 392.cpp
+++ leetcode 392.cpp
@@ -10,3 +10,31 @@ public:
     return (cur == lens);
   }
 };
+
+// Advanced solution
+// 定义第一维为n+1的好处：
+// 1. 预处理的时候不用特判（否则需要特判i < n-1)
+// 2. 输入的t可能是空串，如果第一维开n，那么直接就内存访问越界了
+class Solution {
+public:
+  bool isSubsequence(string s, string t) {
+    int n = t.size();
+    // nxt[i][c]表示大于等于i的下标位置当中，最近的一个字符c的位置。
+    std::vector nxt(n + 1, std::array<int, 26>{});
+    std::fill(nxt[n].begin(), nxt[n].end(), n);
+    // 预处理
+    for (int i = n - 1; i >= 0; i--) {
+      int idx = t[i] - 'a';
+      nxt[i] = nxt[i + 1];
+      nxt[i][idx] = i;
+    }
+    int cur = -1;
+    for (int i = 0; i < s.size(); i++) {
+      int idx = s[i] - 'a';
+      cur = nxt[cur + 1][idx];
+      if (cur == n)
+        return false;
+    }
+    return true;
+  }
+};
