190549f260113f4928745d63fa07c1efa9605b88
GoldenStain <goldenstainxym@gmail.com>
2025-07-01 00:12:43 +0800

    add hashtable solution for leetcode 47.

--- leetcode 47.cpp
+++ leetcode 47.cpp
@@ -25,3 +25,33 @@ public:
     return ans;
   }
 };
+
+// hashtable solution
+class Solution {
+public:
+  vector<vector<int>> permuteUnique(vector<int> &nums) {
+    int n = nums.size();
+    std::unordered_map<int, int> um;
+    for (int i = 0; i < n; i++)
+      um[nums[i]]++;
+    std::vector<std::vector<int>> ans;
+    std::vector<int> tmp(n);
+    auto permuteAll = [&](this auto &self, int step) -> void {
+      if (step == n) {
+        ans.push_back(tmp);
+        return;
+      }
+      // 这里要记得用引用，否则map本身的值不会改变
+      for (auto &pair : um) {
+        if (pair.second == 0)
+          continue;
+        pair.second--;
+        tmp[step] = pair.first;
+        self(step + 1);
+        pair.second++;
+      }
+    };
+    permuteAll(0);
+    return ans;
+  };
+};
