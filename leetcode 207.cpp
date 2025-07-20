dca256be6366c40b026c378f0800f43f0fd1bd67
GoldenStain <goldenstainxym@gmail.com>
2025-07-20 23:05:39 +0800

    add dfs version for leetcode 207.

--- leetcode 207.cpp
+++ leetcode 207.cpp
@@ -27,3 +27,36 @@ public:
     return (cnt_qed == numCourses);
   }
 };
+
+// DFS version
+class Solution {
+public:
+  bool canFinish(int numCourses, vector<vector<int>> &prerequisites) {
+    std::vector edges(numCourses, std::vector<int>{});
+    std::vector<char> vis(numCourses, 0); // 0:没有遍历过，1遍历中,2遍历结束
+    for (auto &p : prerequisites) {
+      edges[p[1]].push_back(p[0]);
+    }
+    std::function<bool(int)> dfs;
+    dfs = [&](int cur) -> bool {
+      if (vis[cur] == 1)
+        return false;
+      vis[cur] = 1;
+      for (auto to_i : edges[cur]) {
+        if (vis[to_i] == 2)
+          continue;
+        if (!dfs(to_i))
+          return false;
+      }
+      vis[cur] = 2;
+      return true;
+    };
+    for (int i = 0; i < numCourses; i++) {
+      if (vis[i] == 2)
+        continue;
+      if (!dfs(i))
+        return false;
+    }
+    return true;
+  }
+};
