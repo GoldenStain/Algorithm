5b8a4ae79eea222ec166893030fcf8fd2678416b
xiaoyumeng02 <xiaoyumeng02@meituan.com>
2025-09-02 18:00:47 +0800

    update for leetcode 85.

--- leetcode 85.cpp
+++ leetcode 85.cpp
@@ -33,3 +33,40 @@ public:
     return ans;
   }
 };
+
+// best solution
+class Solution {
+public:
+  int maximalRectangle(vector<vector<char>> &matrix) {
+    int m = matrix.size(), n = matrix[0].size();
+    std::vector<int> heights(
+        n, 0); // heights[i]: 当前行第i个位置（也就是第i列上，连续1的个数）
+    heights.push_back(-1); // padding.
+    int ans = 0;
+    std::function<int()> computeArea = [&]() -> int {
+      std::vector<int> stk;
+      stk.push_back(-1); // padding
+      int res = 0;
+      for (int i = 0; i <= n; i++) {
+        // 因为padding了，所以这里得是stk.size() > 1
+        while (stk.size() > 1 && heights[stk.back()] >= heights[i]) {
+          int h_now = heights[stk.back()];
+          stk.pop_back();
+          int left = stk.back(), right = i;
+          res = std::max(res, (right - left - 1) * h_now);
+        }
+        stk.push_back(i);
+      }
+      return res;
+    };
+    for (int i = 0; i < m; i++) {
+      // 以当前行为底边
+      for (int j = 0; j < n; j++) {
+        // 更新heights数组
+        heights[j] = (matrix[i][j] == '0') ? 0 : (heights[j] + 1);
+      }
+      ans = std::max(ans, computeArea());
+    }
+    return ans;
+  }
+};
